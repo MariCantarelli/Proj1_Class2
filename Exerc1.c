c093460d24175aeb67ccb7e6f94e175218e36340
Marina Cantarelli <111306797+MariCantarelli@users.noreply.github.com>
2025-02-17 11:53:22 +0000

    Exercicio completo!

--- Exerc1.c
+++ Exerc1.c
@@ -47,10 +47,23 @@ float calcular_a_media(int v[], int n){
     return soma/n;
 }
 
+/* todos os números menores do que a média calculada no item anterior. Neste item,
+apenas imprima os valores na tela*/
+void imprime_num_menores_que_media(int v, int n){
+    float media = calcular_a_media(v, n);
+    for(int i = 0; i < 0; i++){
+        if(v[i] < media){
+            printf("%d ", v[i]);
+        }
+    }
+    printf("\n"); 
+}
+
 int main(){
     int v[10] = {5, 3, 8, 11, 21, 44, 51, 23, 10, 9};
     printf("O maior elemento do vetor: %d\n", encontra_maior(v, 10));
     printf("O menor elemento do vetor: %d\n", encontra_menor(v, 10));
     printf("Quantidade de numeros pares: %d\n", conta_numeros_pares(v, 10));
     printf("A media dos numeros é %.2f\n", calcular_a_media(v, 10));
+    imprime_num_menores_que_media(v,10); 
 }
